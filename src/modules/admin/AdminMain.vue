0e4372bb051ffd2dd13e94aee0705681a0befe39
mccrush <mccrush2027@gmail.com>
2025-02-01 13:24:44 +0700

    Add table

--- src/modules/admin/AdminMain.vue
+++ src/modules/admin/AdminMain.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="border border-2 border-warning p-3">
-    <TheNavbar>
-      <FormSearch />
-    </TheNavbar>
     <h4>AdminMain</h4>
-    <TableMain />
+    <TheNavbar>
+      <FormSearch v-model:searchText="searchText" />
+    </TheNavbar>
+    <div class="row text-center p-2">
+      <div class="col-6">Всего: {{ lengthUsers }}</div>
+      <div class="col-6">Pro: {{ lengthProUsers }}</div>
+    </div>
+    <TableMain :items="searchUsers" />
   </div>
 </template>
 
@@ -15,6 +19,30 @@ import TableMain from './components/table/TableMain.vue'
 
 export default {
   name: 'AdminMain',
-  components: { TheNavbar, FormSearch, TableMain }
+  components: { TheNavbar, FormSearch, TableMain },
+  data() {
+    return {
+      searchText: ''
+    }
+  },
+  computed: {
+    users() {
+      return this.$store.getters.users
+    },
+    searchUsers() {
+      if (this.searchText) {
+        return this.users.filter(item =>
+          item.email.toUpperCase().includes(this.searchText.toUpperCase())
+        )
+      }
+      return this.users
+    },
+    lengthUsers() {
+      return this.users.length
+    },
+    lengthProUsers() {
+      return this.users.filter(item => item.user_metadata.proStatus).length
+    }
+  }
 }
 </script>
--- src/modules/admin/components/table/TableMain.vue
+++ src/modules/admin/components/table/TableMain.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="border border-2 border-success p-3">
-    <h4>TableMain</h4>
+    <table class="table table-striped align-middle mb-0">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">User</th>
+          <th scope="col" class="text-center">proFree</th>
+          <th scope="col" class="text-center">proStatus</th>
+          <th scope="col" class="text-center">startPro</th>
+          <th scope="col" class="text-center">endPro</th>
+          <th scope="col" class="text-center">Last Sig</th>
+          <th scope="col" class="text-center">Бан</th>
+        </tr>
+      </thead>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TableMain'
+  name: 'TableMain',
+  props: {
+    items: Array
+  }
 }
 </script>
